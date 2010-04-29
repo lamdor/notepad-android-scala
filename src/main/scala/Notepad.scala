a996e996060cd8cb32d068ade33f3a5b86a1b36b
Luke Amdor <luke.amdor@gmail.com>
2010-04-28 23:10:58 -0500

    Can now create and edit notes with our own title and body

--- src/main/scala/Notepad.scala
+++ src/main/scala/Notepad.scala
@@ -1,10 +1,12 @@
 package eg.notepad
 
-import _root_.android.app.ListActivity
+import _root_.android.app.{ Activity, ListActivity }
 import _root_.android.os.Bundle
+import _root_.android.content.Intent
 import _root_.android.database.Cursor
-import _root_.android.widget.{ TextView, SimpleCursorAdapter, AdapterView }
+import _root_.android.widget._
 import _root_.android.view.{ Menu, MenuItem, ContextMenu, View }
+import _root_.android.util.Log
 
 object NotepadMenu {
   val INSERT_ID = Menu.FIRST
@@ -14,6 +16,44 @@ object NotepadMenu {
 object NotepadActivities {
   val ACTIVITY_CREATE = 0
   val ACTIVITY_EDIT = 1
+}
+
+class NoteEdit extends Activity {
+  override def onCreate(savedInstanceState: Bundle) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.note_edit)
+
+    val titleText = findViewById(R.id.title).asInstanceOf[EditText]
+    val bodyText = findViewById(R.id.body).asInstanceOf[EditText]
+    val confirmButton = findViewById(R.id.confirm).asInstanceOf[Button]
+
+    val extras = getIntent().getExtras
+    var rowId: Long = 0
+
+    if (extras != null) {
+      val title = extras.getString(NotesDbSchema.KEY_TITLE)
+      val body = extras.getString(NotesDbSchema.KEY_BODY)
+      rowId = extras.getLong(NotesDbSchema.KEY_ROW_ID)
+
+      if (title != null) titleText.setText(title)
+      if (body != null) bodyText.setText(body)
+    }
+
+    confirmButton.setOnClickListener(new View.OnClickListener {
+      override def onClick(view: View) {
+        val bundle = new Bundle
+        bundle.putString(NotesDbSchema.KEY_TITLE, titleText.getText.toString)
+        bundle.putString(NotesDbSchema.KEY_BODY, bodyText.getText.toString)
+        bundle.putLong(NotesDbSchema.KEY_ROW_ID, rowId)
+
+        val i = new Intent
+        i.putExtras(bundle)
+        setResult(Activity.RESULT_OK, i)
+        finish()
+      }
+    })
+
+  }
 }
 
 class Notepad extends ListActivity {
@@ -63,7 +103,38 @@ class Notepad extends ListActivity {
     }
   }
 
+  override def onListItemClick(listView: ListView, view: View, position: Int, id: Long) {
+    super.onListItemClick(listView, view, position, id)
+    val c = currentNotesCursor
+    c.moveToPosition(position)
+    val intent = new Intent(this, classOf[NoteEdit])
+    intent.putExtra(NotesDbSchema.KEY_ROW_ID, id)
+    intent.putExtra(NotesDbSchema.KEY_TITLE, c.getString(c.getColumnIndexOrThrow(NotesDbSchema.KEY_TITLE)))
+    intent.putExtra(NotesDbSchema.KEY_BODY, c.getString(c.getColumnIndexOrThrow(NotesDbSchema.KEY_BODY)))
+    startActivityForResult(intent, NotepadActivities.ACTIVITY_EDIT)
+  }
+
+  override def onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+    super.onActivityResult(resultCode, resultCode, data)
+    val extras = data.getExtras
+
+    val title = extras.getString(NotesDbSchema.KEY_TITLE)
+    val body = extras.getString(NotesDbSchema.KEY_BODY)
+
+    requestCode match {
+      case NotepadActivities.ACTIVITY_CREATE =>
+        dbHelper.createNote(title, body)
+      case NotepadActivities.ACTIVITY_EDIT =>
+        val id = extras.getLong(NotesDbSchema.KEY_ROW_ID)
+        dbHelper.updateNote(id, title, body)
+    }
+
+    fillData
+  }
+
   private def createNote() {
+    val intent = new Intent(this, classOf[NoteEdit])
+    startActivityForResult(intent, NotepadActivities.ACTIVITY_CREATE)
   }
 
   private def fillData() {
