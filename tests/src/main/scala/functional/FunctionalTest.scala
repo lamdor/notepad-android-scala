b55d73cf8464e3325ce41d0c48342bfea18808d7
Luke Amdor <luke.amdor@gmail.com>
2010-04-29 23:07:06 -0500

    Clearing the database before and after our test

--- tests/src/main/scala/functional/FunctionalTest.scala
+++ tests/src/main/scala/functional/FunctionalTest.scala
@@ -6,4 +6,16 @@ import com.jayway.android.robotium.solo.Solo
 
 abstract class FunctionalTest extends ActivityInstrumentationTestCase2[Notepad]("eg.notepad", classOf[Notepad]) {
   lazy val solo = new Solo(getInstrumentation, getActivity)
+  lazy val dbHelper = new DatabaseHelper(getActivity)
+  lazy val db = dbHelper.getWritableDatabase
+
+  override def setUp() {
+    super.setUp()
+    dbHelper.onUpgrade(db, 0, 0)
+  }
+
+  override def tearDown() {
+    super.tearDown()
+    dbHelper.onUpgrade(db, 0, 0)
+  }
 }
